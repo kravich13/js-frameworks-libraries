59231487bb4b57e7a0596aecb07580cf15253aa7
kravich13 <corlack1997@gmail.com>
2020-11-11 17:39:24 +0200

    2 file MD: work with DataBase

---
+++ Practica/4-DataBase/MySQL|MariaDB/2-Работа с M&M/app.js
@@ -0,0 +1,98 @@
+const mysql = require("mysql2")
+
+
+// const connection = mysql.createConnection({
+//     host: "localhost",
+//     user: "root",
+//     database: "nodejs",
+//     password: "rfgkzrfgkz"
+// })
+
+// connection.query("CREATE DATABASE nodejs",
+//     function (err, results) {
+//         if (err) return console.log(err)
+
+//         console.log("База данных создана.")
+// })
+
+// const users = [
+//     ["Vlad", 23],
+//     ["Max", 27],
+//     ["Anna", 26]
+// ]
+// const sql = `INSERT INTO Users(Name, Age) VALUES ?`
+
+// connection.query(sql, [users], function (err, results) {
+//     if (err) return console.log(err)
+
+//     console.log(results)
+// })
+
+
+
+
+//  WHERE
+// const sql = `SELECT * FROM Users WHERE Name = ? AND Age = ?`
+// const filter = ["Anna", 26]
+
+// connection.query(sql, filter, function (err, results) {
+//     if (err) return console.log(err)
+
+//     console.log(results)
+//     // [ TextRow { Id: 3, Name: 'Anna', Age: 26 } ]
+// })
+
+// connection.end()
+
+
+
+// Пулы
+
+const pool = mysql.createPool({
+    connectionLimit: 5,
+    host: "localhost",
+    user: "root",
+    database: "nodejs",
+    password: "rfgkzrfgkz"
+}).promise()
+
+// const sql = "INSERT INTO users (name, age) VALUES(?, ?)"
+// const data = ["Vlad", 23]
+
+// pool.query(sql, data, function (err, results) {
+//   if (err) return console.log(err)
+
+//   console.log(results)
+// })
+ 
+// // получение объектов
+// pool.query("SELECT * FROM users", function (err, results) {
+//     if (err) return console.log(err)
+
+//     console.log(results)
+// })
+
+
+pool.execute("UPDATE users SET age=age+1 WHERE name=?", ["Stan"]) // изменение объектов
+    .then(result => { 
+      console.log(result[0])
+      return pool.execute("SELECT * FROM users") // получение объектов
+    })
+    .then(result => {
+      console.log(result[0])
+      pool.end()
+    })
+    .then( () => {
+      console.log("Пул закрыт.")
+    })
+    .catch( function (err) {
+      console.log(err.message)
+})
+
+
+
+
+
+
+
+
