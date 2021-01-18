763f5331ea76c7fcb0df3b5b542e876b54ba4732
kravich13 <corlack1997@gmail.com>
2021-01-18 08:00:08 +0200

    react

---
+++ Practica/7-React/react-all/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react"
+import "./App.css"
+
+class App extends Component {
+    render () {
+        return (
+            <div className="App">
+                <h1>Привет ещё раз.</h1>
+                <p>Спустя 1 год я взялся за реакт</p>
+            </div>
+        )
+    }
+}
+
+export default App
