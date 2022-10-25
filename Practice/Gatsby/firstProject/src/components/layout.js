566c4ce6ad5bc796667ebd7d91997018723b618d
Vladislav Onatskyi <vladislav.onatskiy@freshcodeit.net>
2022-10-25 16:08:37 +0300

    finished work with Gatsby

--- Practice/Gatsby/firstProject/src/components/layout.js
+++ Practice/Gatsby/firstProject/src/components/layout.js
@@ -14,7 +14,7 @@ import "./layout.css"
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
+    query MyQuery {
       site {
         siteMetadata {
           title
